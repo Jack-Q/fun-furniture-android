4e81c0e1d819ac6e638e7227c3ca50b65ca8afd2
Jack Q <QiaoBo@outlook.com>
2016-11-23 00:47:31 +0800

    init

---
+++ app/src/main/java/com/jackq/funfurniture/AR/IVuforiaApplicationControl.java
@@ -0,0 +1,45 @@
+package com.jackq.funfurniture.AR;
+
+import com.vuforia.State;
+
+/**
+ * Created by jackq on 11/22/16.
+ */
+
+public interface IVuforiaApplicationControl {
+
+
+    // To be called to initialize the trackers
+    boolean doInitTrackers();
+
+
+    // To be called to load the trackers' data
+    boolean doLoadTrackersData();
+
+
+    // To be called to start tracking with the initialized trackers and their
+    // loaded data
+    boolean doStartTrackers();
+
+
+    // To be called to stop the trackers
+    boolean doStopTrackers();
+
+
+    // To be called to destroy the trackers' data
+    boolean doUnloadTrackersData();
+
+
+    // To be called to deinitialize the trackers
+    boolean doDeinitTrackers();
+
+
+    // This callback is called after the Vuforia initialization is complete,
+    // the trackers are initialized, their data loaded and
+    // tracking is ready to start
+    void onInitARDone(VuforiaApplicationException e);
+
+
+    // This callback is called every cycle
+    void onVuforiaUpdate(State state);
+}
