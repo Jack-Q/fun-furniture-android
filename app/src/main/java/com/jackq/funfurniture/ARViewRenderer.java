3b1369797534131f29a38fcf63993daedb4340de
Jack Q <QiaoBo@outlook.com>
2016-11-28 16:23:21 +0800

    abstract renderer class

--- app/src/main/java/com/jackq/funfurniture/ARViewRenderer.java
+++ app/src/main/java/com/jackq/funfurniture/ARViewRenderer.java
@@ -48,71 +48,70 @@ import java.util.Locale;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-class VideoBackgroundShader {
-    private static final String VB_VERTEX_SHADER =
-            "attribute vec4 vertexPosition; " +
-                    "attribute vec2 vertexTexCoord;\n" +
-                    "uniform mat4 projectionMatrix;\n" +
-                    "varying vec2 texCoord;\n" +
-                    "void main()\n" +
-                    "{\n" +
-                    "    gl_Position = projectionMatrix * vertexPosition;\n" +
-                    "    texCoord = vertexTexCoord;\n" +
-                    "}\n";
+abstract class AbstractARViewRenderer extends Renderer {
 
-    private static final String VB_FRAGMENT_SHADER =
-            "precision mediump float;\n" +
-                    "varying vec2 texCoord;\n" +
-                    "uniform sampler2D texSampler2D;\n" +
-                    "void main ()\n" +
-                    "{\n" +
-                    "    gl_FragColor = texture2D(texSampler2D, texCoord);\n" +
-                    "}\n";
+    class VideoBackgroundShader {
+        private static final String VB_VERTEX_SHADER =
+                "attribute vec4 vertexPosition; " +
+                        "attribute vec2 vertexTexCoord;\n" +
+                        "uniform mat4 projectionMatrix;\n" +
+                        "varying vec2 texCoord;\n" +
+                        "void main()\n" +
+                        "{\n" +
+                        "    gl_Position = projectionMatrix * vertexPosition;\n" +
+                        "    texCoord = vertexTexCoord;\n" +
+                        "}\n";
 
-    // Members holds the reference to a shader instance
-    // performed as a container class used with in this file
-    int shaderProgramID = 0;
-    int textureSampler2DHandle = 0;
-    int vertexPositionHandle = 0;
-    int vertexTextureCoordinateHandle = 0;
-    int projectionMatrixHandle = 0;
+        private static final String VB_FRAGMENT_SHADER =
+                "precision mediump float;\n" +
+                        "varying vec2 texCoord;\n" +
+                        "uniform sampler2D texSampler2D;\n" +
+                        "void main ()\n" +
+                        "{\n" +
+                        "    gl_FragColor = texture2D(texSampler2D, texCoord);\n" +
+                        "}\n";
 
-    VideoBackgroundShader() {
+        // Members holds the reference to a shader instance
+        // performed as a container class used with in this file
+        int shaderProgramID = 0;
+        int textureSampler2DHandle = 0;
+        int vertexPositionHandle = 0;
+        int vertexTextureCoordinateHandle = 0;
+        int projectionMatrixHandle = 0;
 
-        shaderProgramID = com.jackq.funfurniture.AR.util.Utilities.createProgramFromShaderSrc(VB_VERTEX_SHADER,
-                VB_FRAGMENT_SHADER);
+        VideoBackgroundShader() {
 
-        // Rendering configuration for video background
-        if (shaderProgramID > 0) {
-            // Activate shader:
-            GLES20.glUseProgram(shaderProgramID);
+            shaderProgramID = com.jackq.funfurniture.AR.util.Utilities.createProgramFromShaderSrc(VB_VERTEX_SHADER,
+                    VB_FRAGMENT_SHADER);
 
-            // Retrieve handle to shader variables
-            vertexPositionHandle = GLES20.glGetAttribLocation(shaderProgramID, "vertexPosition");
-            vertexTextureCoordinateHandle = GLES20.glGetAttribLocation(shaderProgramID, "vertexTexCoord");
-            projectionMatrixHandle = GLES20.glGetUniformLocation(shaderProgramID, "projectionMatrix");
-            textureSampler2DHandle = GLES20.glGetUniformLocation(shaderProgramID, "texSampler2D");
+            // Rendering configuration for video background
+            if (shaderProgramID > 0) {
+                // Activate shader:
+                GLES20.glUseProgram(shaderProgramID);
 
-            // Stop using the program
-            GLES20.glUseProgram(0);
+                // Retrieve handle to shader variables
+                vertexPositionHandle = GLES20.glGetAttribLocation(shaderProgramID, "vertexPosition");
+                vertexTextureCoordinateHandle = GLES20.glGetAttribLocation(shaderProgramID, "vertexTexCoord");
+                projectionMatrixHandle = GLES20.glGetUniformLocation(shaderProgramID, "projectionMatrix");
+                textureSampler2DHandle = GLES20.glGetUniformLocation(shaderProgramID, "texSampler2D");
+
+                // Stop using the program
+                GLES20.glUseProgram(0);
+            }
         }
     }
-}
 
-public class ARViewRenderer extends Renderer {
-    private static final String TAG = "ARViewRenderer";
+    private static final String TAG = "AbstractARViewRenderer";
     private com.vuforia.Renderer vuforiaRenderer;
     private RenderingPrimitives mRenderingPrimitives = null;
     private Activity activity;
     private VuforiaApplicationSession vuforiaAppSession;
-
 
     private boolean isActive;
 
     private int currentView = VIEW.VIEW_SINGULAR;
     private float mNearPlane = -1.0f;
     private float mFarPlane = -1.0f;
-
 
     // Shader user to render the video background on AR mode
     private VideoBackgroundShader videoBackgroundShader = null;
@@ -123,11 +122,9 @@ public class ARViewRenderer extends Renderer {
     private int mScreenHeight = 0;
 
     // Stores orientation
-    private boolean isPortrait = false;
+    protected boolean isPortrait = false;
 
-    private Object3D object3D = null;
-
-    public ARViewRenderer(Activity activity, VuforiaApplicationSession session) {
+    public AbstractARViewRenderer(Activity activity, VuforiaApplicationSession session) {
         // construct Rajawali 3D
         super(activity);
         this.activity = activity;
@@ -182,15 +179,6 @@ public class ARViewRenderer extends Renderer {
         // initRendering();
     }
 
-    @Override
-    public void onOffsetsChanged(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep, int xPixelOffset, int yPixelOffset) {
-
-    }
-
-    @Override
-    public void onTouchEvent(MotionEvent event) {
-
-    }
 
     /**
      * @see org.rajawali3d.view.SurfaceView.RendererDelegate#onSurfaceCreated(GL10, EGLConfig)
@@ -293,142 +281,8 @@ public class ARViewRenderer extends Renderer {
 
     }
 
-    public void setCurrentObject(Object3D object3D) {
-        if (this.object3D != null) {
-            getCurrentScene().removeChild(this.object3D);
-        }
 
-
-        // object3D.setPosition(1, 0, -10);
-        object3D.setScale(200);
-        object3D.setRotY(90);
-        this.object3D = new Object3D();
-        this.object3D.addChild(object3D);
-        // object3D.setVisible(false);
-        getCurrentScene().addChild(this.object3D);
-
-        DirectionalLight mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
-        mLight.setColor(1.0f, 1.0f, 1.0f);
-        mLight.setPower(2);
-
-        // region debug object
-        CoordinateTrident coordinateTrident = new CoordinateTrident();
-        coordinateTrident.setPosition(1, 0, -10);
-        coordinateTrident.setVisible(false);
-        getCurrentScene().addChild(coordinateTrident);
-        GridFloor gridFloor = new GridFloor(100, Color.YELLOW, 2, 10);
-        gridFloor.setPosition(0, 0, -10);
-        gridFloor.setRotZ(90);
-        gridFloor.setVisible(false);
-        getCurrentScene().addChild(gridFloor);
-        gridFloor = new GridFloor(1000, Color.BLUE, 2, 100);
-        gridFloor.setPosition(0, 0, -10);
-        gridFloor.setRotZ(90);
-        gridFloor.setVisible(false);
-        getCurrentScene().addChild(gridFloor);
-        // endregion
-
-    }
-
-    private void onUpdateARViewScene(State state, float[] projectionMatrix) {
-
-
-        // Currently, we only care about single marker, thus only retrieve the first marker
-        if (state.getNumTrackableResults() <= 0) {
-            if (this.object3D != null)
-                this.object3D.setVisible(false);
-            return;
-        }
-        TrackableResult result = state.getTrackableResult(0);
-        // Trackable trackable = result.getTrackable();
-        // Log.d(TAG, "User data of detected marker: " + trackable.getUserData());
-        Matrix44F modelViewMatrix_Vuforia = Tool.convertPose2GLMatrix(result.getPose());
-        float[] modelViewMatrix = modelViewMatrix_Vuforia.getData();
-
-        // from this interface can we acquire the current marker, thus multiple marker is enabled by default
-        // int textureIndex = trackable.getName().equalsIgnoreCase("stones") ? 0 : 1;
-
-        // deal with the modelview and projection matrices
-        float[] modelViewProjection = new float[16];
-
-        // Translation can also be applied in the similar way
-        Matrix.rotateM(modelViewMatrix, 0, 90.0f, 1.0f, 0, 0);
-        // Matrix.scaleM(modelViewMatrix, 0, 2, 2, 2);
-        Matrix.multiplyMM(modelViewProjection, 0, projectionMatrix, 0, modelViewMatrix, 0);
-
-        if (this.object3D != null) {
-            // TODO: update object position
-            object3D.setVisible(true);
-            // object3D.render(getCurrentCamera(), new Matrix4(modelViewMatrix), new Matrix4(projectionMatrix), new Matrix4(), null);
-            String str = "Model View Matrix\n";
-            for (int i = 0; i < 4; i++)
-                str += String.format(Locale.ENGLISH, "%4.2f,%4.2f,%4.2f,%4.2f\n", modelViewMatrix[i], modelViewMatrix[i + 4], modelViewMatrix[i + 8], modelViewMatrix[i + 12]);
-            str += "Projection Matrix\n";
-            for (int i = 0; i < 4; i++)
-                str += String.format(Locale.ENGLISH, "%4.2f,%4.2f,%4.2f,%4.2f\n", projectionMatrix[i], projectionMatrix[i + 4], projectionMatrix[i + 8], projectionMatrix[i + 12]);
-
-            //object3D.setBackSidedobject3D.getScenePosition());
-
-            //object3D.getModelViewProjectionMatrix().setAll(projectionMatrix);
-            transformPositionAndOrientation(modelViewMatrix);
-
-            // object3D.setPosition(mPosition);
-            mPosition.z = -mPosition.z;
-            mPosition.x = -mPosition.x;
-            mPosition.y = -mPosition.y;
-            getCurrentCamera().setPosition(mPosition);
-            object3D.setOrientation(mOrientation);
-            str += "Marker: ";
-            str += mPosition;
-            str += "Object: ";
-            str += object3D.getPosition();
-            final String string = str;
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    TextView textView = (TextView) activity.findViewById(R.id.text_test_surface_output);
-                    textView.setText(string);
-                }
-            });
-        }
-
-    }
-
-    private Vector3 mPosition = new Vector3();
-    private Quaternion mOrientation = new Quaternion();
-    private double[] mModelViewMatrix = new double[16];
-
-    private void transformPositionAndOrientation(float[] modelViewMatrix) {
-        mPosition.setAll(modelViewMatrix[12], -modelViewMatrix[13],
-                -modelViewMatrix[14]);
-        for (int mI = 0; mI < 16; mI++) {
-            mModelViewMatrix[mI] = modelViewMatrix[mI];
-        }
-        mOrientation.fromMatrix(mModelViewMatrix);
-
-        if (isPortrait) {
-            mPosition.setAll(-modelViewMatrix[13], -modelViewMatrix[12],
-                    -modelViewMatrix[14]);
-            double orX = mOrientation.x;
-            mOrientation.x = -mOrientation.y;
-            mOrientation.y = -orX;
-            mOrientation.z = -mOrientation.z;
-        } else {
-            mPosition.setAll(modelViewMatrix[12], -modelViewMatrix[13],
-                    -modelViewMatrix[14]);
-            mOrientation.y = -mOrientation.y;
-            mOrientation.z = -mOrientation.z;
-        }
-    }
-
-    /**
-     * Render the Scene
-     */
-    @Override
-    protected void onRender(long elapsedRealTime, double deltaTime) {
-        // Render Rajawali Scene
-        super.onRender(elapsedRealTime, deltaTime);
-    }
+    abstract void onUpdateARViewScene(State state, float[] projectionMatrix);
 
 
     // Stores the orientation depending on the current resources configuration
@@ -525,8 +379,8 @@ public class ARViewRenderer extends Renderer {
     }
 
 
-    static final float VIRTUAL_FOV_Y_DEGS = 85.0f;
-    static final float M_PI = 3.14159f;
+    private static final float VIRTUAL_FOV_Y_DEGS = 85.0f;
+    private static final float M_PI = 3.14159f;
 
     double getSceneScaleFactor() {
         // Get the y-dimension of the physical camera field of view
@@ -617,5 +471,158 @@ public class ARViewRenderer extends Renderer {
         }
         mScreenWidth = size.x;
         mScreenHeight = size.y;
+    }
+
+
+}
+
+public class ARViewRenderer extends AbstractARViewRenderer {
+    private Vector3 mPosition = new Vector3();
+    private Quaternion mOrientation = new Quaternion();
+    private double[] mModelViewMatrix = new double[16];
+private Activity activity;
+    protected Object3D object3D = null;
+    private static final String TAG = "ARViewRenderer";
+    public ARViewRenderer(Activity activity, VuforiaApplicationSession session) {
+        super(activity, session);
+        this.activity = activity;
+    }
+
+    @Override
+    public void onOffsetsChanged(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep, int xPixelOffset, int yPixelOffset) {
+
+    }
+
+    @Override
+    public void onTouchEvent(MotionEvent event) {
+
+    }
+
+    @Override
+    public void onUpdateARViewScene(State state, float[] projectionMatrix) {
+
+        // Currently, we only care about single marker, thus only retrieve the first marker
+        if (state.getNumTrackableResults() <= 0) {
+            if (this.object3D != null)
+                this.object3D.setVisible(false);
+            return;
+        }
+        TrackableResult result = state.getTrackableResult(0);
+        // Trackable trackable = result.getTrackable();
+        // Log.d(TAG, "User data of detected marker: " + trackable.getUserData());
+        Matrix44F modelViewMatrix_Vuforia = Tool.convertPose2GLMatrix(result.getPose());
+        float[] modelViewMatrix = modelViewMatrix_Vuforia.getData();
+
+        // from this interface can we acquire the current marker, thus multiple marker is enabled by default
+        // int textureIndex = trackable.getName().equalsIgnoreCase("stones") ? 0 : 1;
+
+        // deal with the modelview and projection matrices
+        float[] modelViewProjection = new float[16];
+
+        // Translation can also be applied in the similar way
+        Matrix.rotateM(modelViewMatrix, 0, 90.0f, 1.0f, 0, 0);
+        // Matrix.scaleM(modelViewMatrix, 0, 2, 2, 2);
+        Matrix.multiplyMM(modelViewProjection, 0, projectionMatrix, 0, modelViewMatrix, 0);
+
+        if (this.object3D != null) {
+            // TODO: update object position
+            object3D.setVisible(true);
+            // object3D.render(getCurrentCamera(), new Matrix4(modelViewMatrix), new Matrix4(projectionMatrix), new Matrix4(), null);
+            String str = "Model View Matrix\n";
+            for (int i = 0; i < 4; i++)
+                str += String.format(Locale.ENGLISH, "%4.2f,%4.2f,%4.2f,%4.2f\n", modelViewMatrix[i], modelViewMatrix[i + 4], modelViewMatrix[i + 8], modelViewMatrix[i + 12]);
+            str += "Projection Matrix\n";
+            for (int i = 0; i < 4; i++)
+                str += String.format(Locale.ENGLISH, "%4.2f,%4.2f,%4.2f,%4.2f\n", projectionMatrix[i], projectionMatrix[i + 4], projectionMatrix[i + 8], projectionMatrix[i + 12]);
+
+            //object3D.setBackSidedobject3D.getScenePosition());
+
+            //object3D.getModelViewProjectionMatrix().setAll(projectionMatrix);
+            transformPositionAndOrientation(modelViewMatrix);
+
+            // object3D.setPosition(mPosition);
+            mPosition.z = -mPosition.z;
+            mPosition.x = -mPosition.x;
+            mPosition.y = -mPosition.y;
+            getCurrentCamera().setPosition(mPosition);
+            object3D.setOrientation(mOrientation);
+            str += "Marker: ";
+            str += mPosition;
+            str += "Object: ";
+            str += object3D.getPosition();
+            final String string = str;
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    TextView textView = (TextView) activity.findViewById(R.id.text_test_surface_output);
+                    textView.setText(string);
+                }
+            });
+        }
+
+    }
+
+
+
+    public void setCurrentObject(Object3D object3D) {
+        if (this.object3D != null) {
+            getCurrentScene().removeChild(this.object3D);
+        }
+
+
+        // object3D.setPosition(1, 0, -10);
+        object3D.setScale(200);
+        object3D.setRotY(90);
+        this.object3D = new Object3D();
+        this.object3D.addChild(object3D);
+        // object3D.setVisible(false);
+        getCurrentScene().addChild(this.object3D);
+
+        DirectionalLight mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
+        mLight.setColor(1.0f, 1.0f, 1.0f);
+        mLight.setPower(2);
+
+        // region debug object
+        CoordinateTrident coordinateTrident = new CoordinateTrident();
+        coordinateTrident.setPosition(1, 0, -10);
+        coordinateTrident.setVisible(false);
+        getCurrentScene().addChild(coordinateTrident);
+        GridFloor gridFloor = new GridFloor(100, Color.YELLOW, 2, 10);
+        gridFloor.setPosition(0, 0, -10);
+        gridFloor.setRotZ(90);
+        gridFloor.setVisible(false);
+        getCurrentScene().addChild(gridFloor);
+        gridFloor = new GridFloor(1000, Color.BLUE, 2, 100);
+        gridFloor.setPosition(0, 0, -10);
+        gridFloor.setRotZ(90);
+        gridFloor.setVisible(false);
+        getCurrentScene().addChild(gridFloor);
+        // endregion
+
+    }
+
+
+
+    protected void transformPositionAndOrientation(float[] modelViewMatrix) {
+        mPosition.setAll(modelViewMatrix[12], -modelViewMatrix[13],
+                -modelViewMatrix[14]);
+        for (int mI = 0; mI < 16; mI++) {
+            mModelViewMatrix[mI] = modelViewMatrix[mI];
+        }
+        mOrientation.fromMatrix(mModelViewMatrix);
+
+        if (isPortrait) {
+            mPosition.setAll(-modelViewMatrix[13], -modelViewMatrix[12],
+                    -modelViewMatrix[14]);
+            double orX = mOrientation.x;
+            mOrientation.x = -mOrientation.y;
+            mOrientation.y = -orX;
+            mOrientation.z = -mOrientation.z;
+        } else {
+            mPosition.setAll(modelViewMatrix[12], -modelViewMatrix[13],
+                    -modelViewMatrix[14]);
+            mOrientation.y = -mOrientation.y;
+            mOrientation.z = -mOrientation.z;
+        }
     }
 }
